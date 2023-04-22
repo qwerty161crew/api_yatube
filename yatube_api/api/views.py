2321bcc7d1c8a8c2ccc721935497dc4a69cfd307
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-22 20:53:41 +0300

    удалять может только автор

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,31 +1,25 @@
-from django.shortcuts import render,  get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from posts.models import Post, Group, Comment
 from api.serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import AuthorDeleteOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    # @action(methods=['get'], detail=True)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthenticated)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthenticated, AuthorDeleteOnly)
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, IsAuthenticated)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
-    # @action(methods=['get'], detail=True)
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthenticated)
-
-
-
-
-
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthenticated, AuthorDeleteOnly)
