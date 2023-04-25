5591d21fd071b89e5e0bc7f98ef0d0df99dc48ce
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-25 11:08:05 +0300

    изменил права доступа

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,10 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class AuthorDeleteOnly(BasePermission):
+class AuthorDeleteOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.user == obj.author:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+
+        return obj.author == request.user
