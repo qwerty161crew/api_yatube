d240774a84cb0265c10281839652161419892b3f
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-27 09:37:58 +0300

    почистил лишнее

--- api/urls.py
+++ api/urls.py
@@ -5,17 +5,16 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, GroupViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register('v1/posts', PostViewSet, basename='post')
-router.register('v1/groups', GroupViewSet, basename='group')
-router.register(r'v1/posts/(?P<post_id>\d+)/comments',
+router.register('posts', PostViewSet, basename='post')
+router.register('groups', GroupViewSet, basename='group')
+router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comment')
-print(router.urls)
 
 
 app_name = 'api'
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('', include((router.urls, 'api'))),
+    path('v1/', include((router.urls, 'api'))),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticated
+
 from django.shortcuts import get_object_or_404
 
 from posts.models import Post, Group
@@ -11,11 +11,10 @@ from .permissions import AuthorCreateorDeleteOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsAuthenticated, AuthorCreateorDeleteOnly)
+    permission_classes = (
+        IsAuthenticated, AuthorCreateorDeleteOnly)
 
     def perform_create(self, serializer):
-        # serializer.validated_data["author"] = self.request.user
         serializer.save(author=self.request.user)
 
 
@@ -26,8 +25,8 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsAuthenticated, AuthorCreateorDeleteOnly)
+    permission_classes = (
+        IsAuthenticated, AuthorCreateorDeleteOnly)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
